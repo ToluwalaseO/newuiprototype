07a25ad850fcd1e83fc7a4b9f62a9af3a5a9661f
ToluwalaseO <benjamoseni@gmail.com>
2024-10-21 01:06:29 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -13,14 +13,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <Sidebar />
-      <Header /> {/* Adding Header component */}
-      <Header2 /> 
-      <Dashboard />
-      <InfluencersSection />
-      <CampaignReach />
-      <RadarChartComponent /> {/* Adding Radar Chart Component if needed */}
-      <AgeGenderChart /> {/* Adding Age Gender Chart Component if needed */}
+      <div className="App-sidebar">
+        <Sidebar />
+      </div>
+      <div className="App-header1">
+        <Header /> {/* Adding Header component */}
+      </div>
+      <div className="App-header2">
+        <Header2 />
+      </div>
+      <div className="App-dashboard">
+        <Dashboard />
+      </div>
+      <div className="App-influencers">
+        <InfluencersSection />
+      </div>
+      <div className="App-campaign-reach">
+        <CampaignReach />
+      </div>
+      <div className="App-radar-chart">
+        <RadarChartComponent /> {/* Adding Radar Chart Component if needed */}
+      </div>
+      <div className="App-age-gender-chart">
+        <AgeGenderChart /> {/* Adding Age Gender Chart Component if needed */}
+      </div>
     </div>
   );
 }
--- src/Header2.js
+++ src/Header2.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './header2.css';
 
-const Header = () => {
+const Header2 = () => {
     return (
-        <div className="header">
-            <h1>Blue Chips Chicago</h1>
-            <p>Diam nullam quis nunc at pretium augue</p>
-            <p>Tiktok | Instagram | Facebook</p>
-            <p>Created on August 20, 201</p>
-            <p>Status: Active</p>
+        <div className="header2">
+            <h1 className="header2-title">Blue Chips Chicago</h1>
+            <p className="header2-subtitle">Diam nullam quis nunc at pretium augue</p>
+            <p className="header2-platforms">Tiktok  Instagram  Facebook</p>
+            <p className="header2-created">Created on August 20, 201</p>
+            <p className="header2-status">Status</p>
+            <p className="header2-active">Active</p>
         </div>
     );
 };
 
-export default Header;
- 
+export default Header2;
